4c58dc34c566d3be30aa6f60be15ae7740734eec
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-05-08 12:25:07 +0300

    advanced todo app

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-console.log('app loaded')
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+    	'my first todo item',
+    	'my second todo item',
+    	'my third todo item',
 
-const div = document.querySelector('.container')
+    ],
+    item: 'item'
+  },
 
-console.log(div)
+  methods: {
+  	addTodo() {
+  		this.todos.push(this.item)
+  		this.item = '';
+  	}
+  }
+
+})
+ 
